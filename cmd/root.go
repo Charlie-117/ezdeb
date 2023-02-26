dbb9f9cb56592268f13130d07418761c7c6e519a
Tony Jose <gmoto1179@gmail.com>
2023-02-26 15:12:51 +0530

    feat(ezdeb): configure config file

--- cmd/root.go
+++ cmd/root.go
@@ -7,12 +7,11 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -50,24 +49,20 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+	// Find home directory.
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	configPath := filepath.Join(home, ".ezdeb")
 
-		// Search config in home directory with name ".ezdeb" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".ezdeb")
-	}
+	// Search config in home/.ezdeb directory with name "pkglist" (without extension).
+	viper.AddConfigPath(configPath)
+	viper.SetConfigType("json")
+	viper.SetConfigName("pkglist")
 
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Error: ", err))
 	}
 }
