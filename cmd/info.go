771626c703b92a0d605c383e57bdccefdd48b4a3
Tony Jose <gmoto1179@gmail.com>
2023-02-26 17:34:52 +0530

    feat(info): add info command

---
+++ cmd/info.go
@@ -0,0 +1,65 @@
+/*
+Copyright © 2023 Tony
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// infoCmd represents the info command
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Show information about a particular package",
+	Long: `Usage: ezdeb info <package_name>`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Please provide a package name")
+			return
+		}
+
+		if len(args) > 1 {
+			fmt.Println("Please provide only one package name")
+			return
+		}
+
+		pkgName := args[0]
+
+		packages := viper.Get("packages").([]interface{})
+		for _, pkg := range packages {
+			pkgMap := pkg.(map[string]interface{})
+			if pkgMap["name"].(string) == pkgName {
+				fmt.Println("Package name: ", pkgMap["name"])
+				fmt.Println("Package description: ", pkgMap["description"])
+				fmt.Println("Package source: ", pkgMap["source"])
+				if pkgMap["source"] == "github" {
+					fmt.Println("Package Repository: ", pkgMap["ghuser"], "/", pkgMap["ghrepo"])
+				}
+				if pkgMap["source"] == "website" {
+					fmt.Println("Package link: ", pkgMap["link"])
+				}
+				return
+			}
+		}
+
+		fmt.Println("Package not found")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(infoCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// infoCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// infoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
