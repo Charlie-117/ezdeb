2366ed2e7e0dfceeb1abbfc9cfddc900206311e2
Tony Jose <gmoto1179@gmail.com>
2023-03-11 22:43:22 +0530

    feat(clearLogs): implement clearLogs command
    - resets the log file (truncates)

---
+++ cmd/clearLogs.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2023 Tony
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// clearLogsCmd represents the clearLogs command
+var clearLogsCmd = &cobra.Command{
+	Use:   "clearLogs",
+	Short: "Clear logs",
+	Long: `Usage: ezdeb clearLogs`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Clearings logs...")
+
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println("failed to get home directory")
+			return
+		}
+
+		logFile := filepath.Join(homeDir, ".ezdeb", "ezdeb.log")
+
+		// replace the logs file with empty file
+		err = os.Truncate(logFile, 0)
+		if err != nil {
+			fmt.Println("Failed to clear log file")
+			return
+		}
+
+		fmt.Println("Logs cleared")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(clearLogsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// clearLogsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// clearLogsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
